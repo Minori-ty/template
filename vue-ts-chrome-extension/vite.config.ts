3c55b13f1627b9dafa9403ca62474df4f73032b4
守夜人 <1229248506@qq.com>
2024-03-07 23:19:10 +0800

    fix: 修改配置

--- vue-ts-chrome-extension/vite.config.ts
+++ vue-ts-chrome-extension/vite.config.ts
@@ -1,22 +1,22 @@
-import path from "path";
-import { defineConfig } from "vite";
-import browserExtension from "vite-plugin-web-extension";
-import vue from "@vitejs/plugin-vue";
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import { defineConfig } from 'vite'
+import browserExtension from 'vite-plugin-web-extension'
 
 function root(...paths: string[]): string {
-  return path.resolve(__dirname, ...paths);
+    return path.resolve(__dirname, ...paths)
 }
 
 export default defineConfig({
-  build: {
-    outDir: root("dist"),
-    emptyOutDir: true,
-  },
-  plugins: [
-    vue(),
-    browserExtension({
-      manifest: "src/manifest.json",
-      browser: process.env.TARGET ?? "chrome",
-    }),
-  ],
-});
+    build: {
+        outDir: root('dist'),
+        emptyOutDir: true,
+    },
+    plugins: [
+        vue(),
+        browserExtension({
+            manifest: 'src/manifest.json',
+            browser: process.env.TARGET ?? 'chrome',
+        }),
+    ],
+})
