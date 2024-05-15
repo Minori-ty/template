22c7ce818672a7627314d7906d61882424dd893c
天平 <1229248506@qq.com>
2024-05-15 18:59:41 +0800

    feat(axum): 添加枚举

--- axum/src/models/user.rs
+++ axum/src/models/user.rs
@@ -4,5 +4,21 @@ use serde::{Deserialize, Serialize};
 pub struct User {
     pub username: String,
     pub age: u8,
-    pub sex: u8,
+    pub sex: Sex,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Sex {
+    Male = 0,
+    Female = 1,
+}
+
+impl From<u8> for Sex {
+    fn from(num: u8) -> Self {
+        match num {
+            0 => Sex::Male,
+            1 => Sex::Female,
+            _ => panic!("Invalid value for Sex"),
+        }
+    }
 }
--- axum/src/services/user.rs
+++ axum/src/services/user.rs
@@ -1,10 +1,11 @@
 use crate::models;
 use crate::models::common::{create_response, Response};
+use crate::models::user::Sex;
 
 pub fn create_user() -> Response<models::user::User> {
     let user = models::user::User {
         age: 20,
-        sex: 0,
+        sex: Sex::Female,
         username: String::from("张三"),
     };
     let res = create_response(user);
